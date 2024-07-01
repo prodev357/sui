prodev357/sui

commit 090e222490cae5f47278857fc0126d05f471f134

[move-ide] Added support for enums (#18124)
## Description 

This PR adds enums support to the Move analyzer. 

In this PR, I also sneaked a fix for symbols merging, which was not very
robust when multiple packages are built within the same IDE instance.
It's much more robust to keep symbols on the per-package basis, even if
it's more memory-intensive.

## Test plan 

All existing and newly added tests must pass.

external-crates/move/crates/move-analyzer/src/completion.rs
@@ -512,6 +512,7 @@ pub fn on_completion_request(
     ide_files_root: VfsPath,
     pkg_dependencies: Arc<Mutex<BTreeMap<PathBuf, PrecompiledPkgDeps>>>,
 ) {
+    let symbols_map = &context.symbols.lock().unwrap();
     eprintln!("handling completion request");
     let parameters = serde_json::from_value::<CompletionParams>(request.params.clone())
         .expect("could not deserialize completion request");
@@ -533,10 +534,19 @@ pub fn on_completion_request(
                 LintLevel::None,
             ) {
                 Ok((Some(symbols), _)) => completion_items(pos, &path, &symbols),
-                _ => completion_items(pos, &path, &context.symbols.lock().unwrap()),
+                _ => {
+                    if let Some(symbols) = symbols_map.get(&pkg_path) {
+                        completion_items(pos, &path, symbols)
+                    } else {
+                        vec![]
+                    }
+                }
             }
         }
-        None => completion_items(pos, &path, &context.symbols.lock().unwrap()),
+        None => {
+            eprintln!("failed completion for {:?} (package root not found)", path);
+            vec![]
+        }
     };
 
     let result = serde_json::to_value(items).expect("could not serialize completion response");

external-crates/move/crates/move-analyzer/src/inlay_hints.rs
@@ -3,18 +3,18 @@
 
 use crate::{
     context::Context,
-    symbols::{on_hover_markup, type_to_ide_string, DefInfo, Symbols},
+    symbols::{on_hover_markup, type_to_ide_string, DefInfo, SymbolicatorRunner, Symbols},
 };
 use lsp_server::Request;
 use lsp_types::{
-    InlayHint, InlayHintKind, InlayHintLabel, InlayHintLabelPart, InlayHintParams,
-    InlayHintTooltip, Position,
+    InlayHint, InlayHintKind, InlayHintLabel, InlayHintLabelPart, InlayHintParams, InlayHintTooltip,
 };
 
 use move_compiler::{naming::ast as N, shared::Identifier};
+use std::path::PathBuf;
 
 /// Handles inlay hints request of the language server
-pub fn on_inlay_hint_request(context: &Context, request: &Request, symbols: &Symbols) {
+pub fn on_inlay_hint_request(context: &Context, request: &Request) {
     let parameters = serde_json::from_value::<InlayHintParams>(request.params.clone())
         .expect("could not deserialize inlay hints request");
 
@@ -23,46 +23,7 @@ pub fn on_inlay_hint_request(context: &Context, request: &Request, symbols: &Sym
         "inlay_hints_request (types: {}): {:?}",
         context.inlay_type_hints, fpath
     );
-    let mut hints: Vec<InlayHint> = vec![];
-
-    if context.inlay_type_hints {
-        if let Some(file_defs) = symbols.file_mods.get(&fpath) {
-            for mod_defs in file_defs {
-                for untyped_def_loc in mod_defs.untyped_defs() {
-                    let start_position = symbols.files.start_position(untyped_def_loc);
-                    if let Some(DefInfo::Local(n, t, _, _)) = symbols.def_info(untyped_def_loc) {
-                        let position = Position {
-                            line: start_position.line_offset() as u32,
-                            character: start_position.column_offset() as u32 + n.len() as u32,
-                        };
-                        let colon_label = InlayHintLabelPart {
-                            value: ": ".to_string(),
-                            tooltip: None,
-                            location: None,
-                            command: None,
-                        };
-                        let type_label = InlayHintLabelPart {
-                            value: type_to_ide_string(t, /* verbose */ true),
-                            tooltip: None,
-                            location: None,
-                            command: None,
-                        };
-                        let h = InlayHint {
-                            position,
-                            label: InlayHintLabel::LabelParts(vec![colon_label, type_label]),
-                            kind: Some(InlayHintKind::TYPE),
-                            text_edits: None,
-                            tooltip: additional_hint_info(t, symbols),
-                            padding_left: None,
-                            padding_right: None,
-                            data: None,
-                        };
-                        hints.push(h);
-                    }
-                }
-            }
-        };
-    }
+    let hints = inlay_hints(context, fpath).unwrap_or_default();
 
     let response = lsp_server::Response::new_ok(request.id.clone(), hints);
     if let Err(err) = context
@@ -72,6 +33,45 @@ pub fn on_inlay_hint_request(context: &Context, request: &Request, symbols: &Sym
     {
         eprintln!("could not send inlay thing response: {:?}", err);
     }
+}
+
+fn inlay_hints(context: &Context, fpath: PathBuf) -> Option<Vec<InlayHint>> {
+    let symbols_map = &context.symbols.lock().ok()?;
+    let mut hints: Vec<InlayHint> = vec![];
+    let symbols =
+        SymbolicatorRunner::root_dir(&fpath).and_then(|pkg_path| symbols_map.get(&pkg_path))?;
+    let file_defs = symbols.file_mods.get(&fpath)?;
+    for mod_defs in file_defs {
+        for untyped_def_loc in mod_defs.untyped_defs() {
+            let start_position = symbols.files.start_position(untyped_def_loc);
+            if let DefInfo::Local(_, t, _, _, _) = symbols.def_info(untyped_def_loc)? {
+                let colon_label = InlayHintLabelPart {
+                    value: ": ".to_string(),
+                    tooltip: None,
+                    location: None,
+                    command: None,
+                };
+                let type_label = InlayHintLabelPart {
+                    value: type_to_ide_string(t, /* verbose */ true),
+                    tooltip: None,
+                    location: None,
+                    command: None,
+                };
+                let h = InlayHint {
+                    position: start_position.into(),
+                    label: InlayHintLabel::LabelParts(vec![colon_label, type_label]),
+                    kind: Some(InlayHintKind::TYPE),
+                    text_edits: None,
+                    tooltip: additional_hint_info(t, symbols),
+                    padding_left: None,
+                    padding_right: None,
+                    data: None,
+                };
+                hints.push(h);
+            }
+        }
+    }
+    Some(hints)
 }
 
 /// Helper function to compute additional optional info for the hint.
@@ -94,22 +94,15 @@ fn additional_hint_info(sp!(_, t): &N::Type, symbols: &Symbols) -> Option<InlayH
         return None;
     };
 
-    let Some(mod_defs) = symbols
+    let mod_defs = symbols
         .file_mods
         .values()
         .flatten()
-        .find(|m| m.ident() == &mod_ident.value)
-    else {
-        return None;
-    };
+        .find(|m| m.ident() == &mod_ident.value)?;
 
-    let Some(struct_def) = mod_defs.structs().get(&struct_name.value()) else {
-        return None;
-    };
+    let struct_def = mod_defs.structs().get(&struct_name.value())?;
 
-    let Some(struct_def_info) = symbols.def_info(&struct_def.name_start()) else {
-        return None;
-    };
+    let struct_def_info = symbols.def_info(&struct_def.name_loc)?;
 
     Some(InlayHintTooltip::MarkupContent(on_hover_markup(
         struct_def_info,

external-crates/move/crates/move-analyzer/src/analyzer.rs
@@ -44,7 +44,7 @@ pub fn run() {
     );
 
     let (connection, io_threads) = Connection::stdio();
-    let symbols = Arc::new(Mutex::new(symbols::empty_symbols()));
+    let symbols_map = Arc::new(Mutex::new(BTreeMap::new()));
     let pkg_deps = Arc::new(Mutex::new(
         BTreeMap::<PathBuf, symbols::PrecompiledPkgDeps>::new(),
     ));
@@ -135,7 +135,7 @@ pub fn run() {
 
     let symbolicator_runner = symbols::SymbolicatorRunner::new(
         ide_files_root.clone(),
-        symbols.clone(),
+        symbols_map.clone(),
         pkg_deps.clone(),
         diag_sender,
         lint,
@@ -154,15 +154,15 @@ pub fn run() {
                 p.as_path(),
                 lint,
             ) {
-                let mut old_symbols = symbols.lock().unwrap();
-                (*old_symbols).merge(new_symbols);
+                let mut old_symbols_map = symbols_map.lock().unwrap();
+                old_symbols_map.insert(p, new_symbols);
             }
         }
     }
 
     let context = Context {
         connection,
-        symbols: symbols.clone(),
+        symbols: symbols_map.clone(),
         inlay_type_hints: initialize_params
             .initialization_options
             .as_ref()
@@ -292,22 +292,22 @@ fn on_request(
             on_completion_request(context, request, ide_files_root.clone(), pkg_dependencies)
         }
         lsp_types::request::GotoDefinition::METHOD => {
-            symbols::on_go_to_def_request(context, request, &context.symbols.lock().unwrap());
+            symbols::on_go_to_def_request(context, request);
         }
         lsp_types::request::GotoTypeDefinition::METHOD => {
-            symbols::on_go_to_type_def_request(context, request, &context.symbols.lock().unwrap());
+            symbols::on_go_to_type_def_request(context, request);
         }
         lsp_types::request::References::METHOD => {
-            symbols::on_references_request(context, request, &context.symbols.lock().unwrap());
+            symbols::on_references_request(context, request);
         }
         lsp_types::request::HoverRequest::METHOD => {
-            symbols::on_hover_request(context, request, &context.symbols.lock().unwrap());
+            symbols::on_hover_request(context, request);
         }
         lsp_types::request::DocumentSymbolRequest::METHOD => {
-            symbols::on_document_symbol_request(context, request, &context.symbols.lock().unwrap());
+            symbols::on_document_symbol_request(context, request);
         }
         lsp_types::request::InlayHintRequest::METHOD => {
-            inlay_hints::on_inlay_hint_request(context, request, &context.symbols.lock().unwrap());
+            inlay_hints::on_inlay_hint_request(context, request);
         }
         lsp_types::request::Shutdown::METHOD => {
             eprintln!("Shutdown request received");

external-crates/move/crates/move-analyzer/src/compiler_info.rs
@@ -12,6 +12,11 @@ pub struct CompilerInfo {
     pub macro_info: BTreeMap<Loc, CI::MacroCallInfo>,
     pub expanded_lambdas: BTreeSet<Loc>,
     pub autocomplete_info: BTreeMap<FileHash, BTreeMap<Loc, CI::AutocompleteInfo>>,
+    /// Locations of binders in enum variants that are expanded from an ellipsis (and should
+    /// not be displayed in any way by the IDE)
+    pub ellipsis_binders: BTreeSet<Loc>,
+    /// Locations of guard expressions
+    pub guards: BTreeMap<FileHash, BTreeSet<Loc>>,
 }
 
 impl CompilerInfo {
@@ -54,7 +59,7 @@ impl CompilerInfo {
                     // TODO: Not much to do with this yet.
                 }
                 CI::IDEAnnotation::EllipsisMatchEntries(_) => {
-                    // TODO: Not much to do with this yet.
+                    self.ellipsis_binders.insert(loc);
                 }
             }
         }
@@ -82,5 +87,13 @@ impl CompilerInfo {
                 }
             })
         })
+    }
+
+    pub fn inside_guard(&self, fhash: FileHash, loc: &Loc, gloc: &Loc) -> bool {
+        self.guards
+            .get(&fhash)
+            .and_then(|guard_locs| guard_locs.get(gloc))
+            .is_some()
+            && gloc.contains(loc)
     }
 }

external-crates/move/crates/move-analyzer/src/context.rs
@@ -4,14 +4,18 @@
 
 use crate::symbols::Symbols;
 use lsp_server::Connection;
-use std::sync::{Arc, Mutex};
+use std::{
+    collections::BTreeMap,
+    path::PathBuf,
+    sync::{Arc, Mutex},
+};
 
 /// The context within which the language server is running.
 pub struct Context {
     /// The connection with the language server's client.
     pub connection: Connection,
     /// Symbolication information
-    pub symbols: Arc<Mutex<Symbols>>,
+    pub symbols: Arc<Mutex<BTreeMap<PathBuf, Symbols>>>,
     /// Are inlay type hints enabled?
     pub inlay_type_hints: bool,
 }

commit 0d352596ad123f2871c9727de31ddb860fb7d090

[move-ide] Fixed inlay type hints (#18478)
## Description 

This PR fixes a problem with inlay type hints introduced in a previous
PR (https://github.com/MystenLabs/sui/pull/18124)

Not only inlay type hints could no longer be disabled, they were also
displayed in the wrong position.

This PR also changes the `move-analyzer` binary installation process to
enable the bundled version overwriting the locally installed one if they
both have the same version but different commit sha. This allows a
`move-analyzer` fix to be pushed with the new version of the extension
without bumping `move-analyzer`'s version number (which is difficult as
it's really the Sui release version number)

## Test plan 

Tested that the installation procedure works as expected, that the inlay
type hints are displayed correctly, and that they can be
enabled/disabled

external-crates/move/crates/move-analyzer/editors/code/src/context.ts
@@ -14,13 +14,17 @@ import { log } from './log';
 import { assert } from 'console';
 import { IndentAction } from 'vscode';
 
-function semanticVersion(path: string, args?: readonly string[]): semver.SemVer | null {
+function version(path: string, args?: readonly string[]): string | null {
     const versionString = childProcess.spawnSync(
         path, args, { encoding: 'utf8' },
     );
-    if (versionString.stdout) {
+    return versionString.stdout;
+}
+
+function semanticVersion(versionString: string | null): semver.SemVer | null {
+    if (versionString !== null) {
         // Version string looks as follows: 'COMMAND_NAME SEMVER-SHA'
-        const versionStringWords = versionString.stdout.split(' ', 2);
+        const versionStringWords = versionString.split(' ', 2);
         if (versionStringWords.length < 2) {
             return null;
         }
@@ -35,6 +39,33 @@ function semanticVersion(path: string, args?: readonly string[]): semver.SemVer 
 
     }
     return null;
+}
+
+function shouldInstall(bundledVersionString: string | null,
+                       bundledVersion: semver.SemVer | null,
+                       highestVersionString: string | null,
+                       highestVersion: semver.SemVer | null): boolean {
+    if (bundledVersionString === null || bundledVersion === null) {
+        log.info('No bundled binary');
+        return false;
+    }
+    if (highestVersionString === null || highestVersion === null) {
+        log.info(`Installing bundled move-analyzer as no existing version found: v${bundledVersion.version}`);
+        return true;
+    }
+    if (semver.gt(bundledVersion, highestVersion)) {
+        log.info(`Installing bundled move-analyzer as the highest version available: v${bundledVersion.version}`);
+        return true;
+    }
+    if (semver.eq(bundledVersion, highestVersion) &&
+        bundledVersionString !== highestVersionString) {
+        // Bundled version is the same as the highest installed one,
+        // but the entire version string including commit sha is different
+        // in which case favor the bundled one as it may contain a patch
+        log.info(`Installing bundled move-analyzer equal to the highest version available: v${bundledVersion.version}`);
+        return true;
+    }
+    return false;
 }
 
 /** Information passed along to each VS Code command defined by this extension. */
@@ -247,15 +278,15 @@ export class Context {
                 () => false,
             );
             if (serverPathExists) {
-                log.info(`deleting existing move-analyzer binary at '${this.configuration.defaultServerPath}'`);
+                log.info(`Deleting existing move-analyzer binary at '${this.configuration.defaultServerPath}'`);
                 await vscode.workspace.fs.delete(this.configuration.defaultServerPath);
             }
          } else {
-            log.info(`creating directory for move-analyzer binary at '${this.configuration.defaultServerDir}'`);
+            log.info(`Creating directory for move-analyzer binary at '${this.configuration.defaultServerDir}'`);
             await vscode.workspace.fs.createDirectory(this.configuration.defaultServerDir);
          }
 
-         log.info(`copying move-analyzer binary to '${this.configuration.defaultServerPath}'`);
+         log.info(`Copying move-analyzer binary to '${this.configuration.defaultServerPath}'`);
          await vscode.workspace.fs.copy(bundledServerPath, this.configuration.defaultServerPath);
     }
 
@@ -297,14 +328,17 @@ export class Context {
         const bundledServerPath = vscode.Uri.joinPath(extensionContext.extensionUri,
                                                     'language-server',
                                                     this.configuration.serverName);
-        const bundledVersion = semanticVersion(bundledServerPath.fsPath, serverVersionArgs);
-        log.info(`bundled version: ${bundledVersion}`);
+        const bundledVersionString = version(bundledServerPath.fsPath, serverVersionArgs);
+        const bundledVersion = semanticVersion(bundledVersionString);
+        log.info(`Bundled version: ${bundledVersion}`);
 
-        const standaloneVersion = semanticVersion(this.configuration.serverPath, serverVersionArgs);
-        log.info(`standalone version: ${standaloneVersion}`);
+        const standaloneVersionString = version(this.configuration.serverPath, serverVersionArgs);
+        const standaloneVersion = semanticVersion(standaloneVersionString);
+        log.info(`Standalone version: ${standaloneVersion}`);
 
-        const cliVersion = semanticVersion(this.configuration.suiPath, cliVersionArgs);
-        log.info(`cli version: ${cliVersion}`);
+        const cliVersionString = version(this.configuration.suiPath, cliVersionArgs);
+        const cliVersion = semanticVersion(cliVersionString);
+        log.info(`CLI version: ${cliVersion}`);
 
         if (this.configuration.serverPath !== this.configuration.defaultServerPath.fsPath) {
             // User has overwritten default server path (need to compare paths as comparing URIs fails
@@ -331,8 +365,10 @@ export class Context {
 
         // What's the highest version installed? Also track path and arguments to run analyzer
         // with the highest version
+        let highestVersionString = null;
         let highestVersion = null;
         if (standaloneVersion !== null) {
+            highestVersionString = standaloneVersionString;
             highestVersion = standaloneVersion;
             this.resolvedServerPath = this.configuration.serverPath;
             this.resolvedServerArgs = serverArgs;
@@ -341,6 +377,7 @@ export class Context {
         }
 
         if (cliVersion !== null && (highestVersion === null || semver.gt(cliVersion, highestVersion))) {
+            highestVersionString = cliVersionString;
             highestVersion = cliVersion;
             this.resolvedServerPath = this.configuration.suiPath;
             this.resolvedServerArgs = cliArgs;
@@ -348,12 +385,10 @@ export class Context {
                     ` at '${this.resolvedServerPath}' as the highest one`);
         }
 
-        if (bundledVersion !== null && (highestVersion === null || semver.gt(bundledVersion, highestVersion))) {
+        if (shouldInstall(bundledVersionString, bundledVersion, highestVersionString, highestVersion)) {
             highestVersion = bundledVersion;
             this.resolvedServerPath = this.configuration.defaultServerPath.fsPath;
             this.resolvedServerArgs = serverArgs;
-            log.info(`Setting v${bundledVersion.version} of bundled move-analyzer ` +
-                    ` at '${this.resolvedServerPath}' as the highest one and installing it`);
             await this.installBundledBinary(bundledServerPath);
             log.info('Successfuly installed move-analyzer');
         }

external-crates/move/crates/move-analyzer/src/completion.rs
@@ -585,10 +585,12 @@ fn completion_items(pos: Position, path: &Path, symbols: &Symbols) -> Vec<Comple
             }
             Some(Tok::Period) => {
                 items = dot(symbols, path, &pos);
-                if !items.is_empty() {
-                    // found dot completions - do not look for any other
-                    only_custom_items = true;
-                }
+                // whether completions have been found for the dot or not
+                // it makes no sense to try offering "dumb" autocompletion
+                // options here as they will not fit (an example would
+                // be dot completion of u64 variable without any methods
+                // with u64 receiver being visible)
+                only_custom_items = true;
             }
 
             Some(Tok::ColonColon) => {

external-crates/move/crates/move-analyzer/src/inlay_hints.rs
@@ -7,7 +7,8 @@ use crate::{
 };
 use lsp_server::Request;
 use lsp_types::{
-    InlayHint, InlayHintKind, InlayHintLabel, InlayHintLabelPart, InlayHintParams, InlayHintTooltip,
+    InlayHint, InlayHintKind, InlayHintLabel, InlayHintLabelPart, InlayHintParams,
+    InlayHintTooltip, Position,
 };
 
 use move_compiler::{naming::ast as N, shared::Identifier};
@@ -23,7 +24,11 @@ pub fn on_inlay_hint_request(context: &Context, request: &Request) {
         "inlay_hints_request (types: {}): {:?}",
         context.inlay_type_hints, fpath
     );
-    let hints = inlay_hints(context, fpath).unwrap_or_default();
+    let hints = if context.inlay_type_hints {
+        inlay_hints(context, fpath).unwrap_or_default()
+    } else {
+        vec![]
+    };
 
     let response = lsp_server::Response::new_ok(request.id.clone(), hints);
     if let Err(err) = context
@@ -44,7 +49,11 @@ fn inlay_hints(context: &Context, fpath: PathBuf) -> Option<Vec<InlayHint>> {
     for mod_defs in file_defs {
         for untyped_def_loc in mod_defs.untyped_defs() {
             let start_position = symbols.files.start_position(untyped_def_loc);
-            if let DefInfo::Local(_, t, _, _, _) = symbols.def_info(untyped_def_loc)? {
+            if let DefInfo::Local(n, t, _, _, _) = symbols.def_info(untyped_def_loc)? {
+                let position = Position {
+                    line: start_position.line_offset() as u32,
+                    character: start_position.column_offset() as u32 + n.len() as u32,
+                };
                 let colon_label = InlayHintLabelPart {
                     value: ": ".to_string(),
                     tooltip: None,
@@ -58,7 +67,7 @@ fn inlay_hints(context: &Context, fpath: PathBuf) -> Option<Vec<InlayHint>> {
                     command: None,
                 };
                 let h = InlayHint {
-                    position: start_position.into(),
+                    position,
                     label: InlayHintLabel::LabelParts(vec![colon_label, type_label]),
                     kind: Some(InlayHintKind::TYPE),
                     text_edits: None,
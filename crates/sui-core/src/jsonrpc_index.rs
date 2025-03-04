prodev357/sui

commit 0269b42498befb8f4cf628dd16b277d2e9d250dc

[typed store] iterators: deprecate skip_to/skip_prior_to/skip_to_last/reverse methods (#21289)
## Description 
deprecates skip_to/skip_prior_to/skip_to_last/reverse methods


---

## Release notes

Check each box that your changes affect. If none of the boxes relate to
your changes, release notes aren't required.

For each box you select, include information after the relevant heading
that describes the impact of your changes that a user might notice and
any actions they must take to implement updates.

- [ ] Protocol: 
- [ ] Nodes (Validators and Full nodes): 
- [ ] gRPC:
- [ ] JSON-RPC: 
- [ ] GraphQL: 
- [ ] CLI: 
- [ ] Rust SDK:

crates/sui-core/src/jsonrpc_index.rs
@@ -417,9 +417,11 @@ impl IndexStore {
         };
         let next_sequence_number = tables
             .transaction_order
-            .unbounded_iter()
-            .skip_to_last()
+            .reversed_safe_iter_with_bounds(None, None)
+            .expect("failed to initialize indexes")
             .next()
+            .transpose()
+            .expect("failed to initialize indexes")
             .map(|(seq, _)| seq + 1)
             .unwrap_or(0)
             .into();
@@ -823,22 +825,26 @@ impl IndexStore {
                 error: UserInputError::Unsupported(format!("{:?}", filter)),
             }),
             None => {
-                let iter = self.tables.transaction_order.unbounded_iter();
-
                 if reverse {
-                    let iter = iter
-                        .skip_prior_to(&cursor.unwrap_or(TxSequenceNumber::MAX))?
-                        .reverse()
+                    let iter = self
+                        .tables
+                        .transaction_order
+                        .reversed_safe_iter_with_bounds(
+                            None,
+                            Some(cursor.unwrap_or(TxSequenceNumber::MAX)),
+                        )?
                         .skip(usize::from(cursor.is_some()))
-                        .map(|(_, digest)| digest);
+                        .map(|result| result.map(|(_, digest)| digest));
                     if let Some(limit) = limit {
-                        Ok(iter.take(limit).collect())
+                        Ok(iter.take(limit).collect::<Result<Vec<_>, _>>()?)
                     } else {
-                        Ok(iter.collect())
+                        Ok(iter.collect::<Result<Vec<_>, _>>()?)
                     }
                 } else {
-                    let iter = iter
-                        .skip_to(&cursor.unwrap_or(TxSequenceNumber::MIN))?
+                    let iter = self
+                        .tables
+                        .transaction_order
+                        .iter_with_bounds(Some(cursor.unwrap_or(TxSequenceNumber::MIN)), None)
                         .skip(usize::from(cursor.is_some()))
                         .map(|(_, digest)| digest);
                     if let Some(limit) = limit {
@@ -861,11 +867,14 @@ impl IndexStore {
     ) -> SuiResult<Vec<TransactionDigest>> {
         Ok(if reverse {
             let iter = index
-                .unbounded_iter()
-                .skip_prior_to(&(key.clone(), cursor.unwrap_or(TxSequenceNumber::MAX)))?
-                .reverse()
+                .reversed_safe_iter_with_bounds(
+                    None,
+                    Some((key.clone(), cursor.unwrap_or(TxSequenceNumber::MAX))),
+                )?
                 // skip one more if exclusive cursor is Some
                 .skip(usize::from(cursor.is_some()))
+                .collect::<Result<Vec<_>, _>>()?
+                .into_iter()
                 .take_while(|((id, _), _)| *id == key)
                 .map(|(_, digest)| digest);
             if let Some(limit) = limit {
@@ -875,8 +884,10 @@ impl IndexStore {
             }
         } else {
             let iter = index
-                .unbounded_iter()
-                .skip_to(&(key.clone(), cursor.unwrap_or(TxSequenceNumber::MIN)))?
+                .iter_with_bounds(
+                    Some((key.clone(), cursor.unwrap_or(TxSequenceNumber::MIN))),
+                    None,
+                )
                 // skip one more if exclusive cursor is Some
                 .skip(usize::from(cursor.is_some()))
                 .take_while(|((id, _), _)| *id == key)
@@ -995,13 +1006,15 @@ impl IndexStore {
                 .unwrap_or(if reverse { max_string } else { "".to_string() });
 
         let key = (package, module_val, function_val, cursor_val);
-        let iter = self.tables.transactions_by_move_function.unbounded_iter();
         Ok(if reverse {
-            let iter = iter
-                .skip_prior_to(&key)?
-                .reverse()
+            let iter = self
+                .tables
+                .transactions_by_move_function
+                .reversed_safe_iter_with_bounds(None, Some(key))?
                 // skip one more if exclusive cursor is Some
                 .skip(usize::from(cursor.is_some()))
+                .collect::<Result<Vec<_>, _>>()?
+                .into_iter()
                 .take_while(|((id, m, f, _), _)| {
                     *id == package
                         && module.as_ref().map(|x| x == m).unwrap_or(true)
@@ -1014,8 +1027,10 @@ impl IndexStore {
                 iter.collect()
             }
         } else {
-            let iter = iter
-                .skip_to(&key)?
+            let iter = self
+                .tables
+                .transactions_by_move_function
+                .iter_with_bounds(Some(key), None)
                 // skip one more if exclusive cursor is Some
                 .skip(usize::from(cursor.is_some()))
                 .take_while(|((id, m, f, _), _)| {
@@ -1068,19 +1083,18 @@ impl IndexStore {
         Ok(if descending {
             self.tables
                 .event_order
-                .unbounded_iter()
-                .skip_prior_to(&(tx_seq, event_seq))?
-                .reverse()
+                .reversed_safe_iter_with_bounds(None, Some((tx_seq, event_seq)))?
                 .take(limit)
-                .map(|((_, event_seq), (digest, tx_digest, time))| {
-                    (digest, tx_digest, event_seq, time)
+                .map(|result| {
+                    result.map(|((_, event_seq), (digest, tx_digest, time))| {
+                        (digest, tx_digest, event_seq, time)
+                    })
                 })
-                .collect()
+                .collect::<Result<Vec<_>, _>>()?
         } else {
             self.tables
                 .event_order
-                .unbounded_iter()
-                .skip_to(&(tx_seq, event_seq))?
+                .iter_with_bounds(Some((tx_seq, event_seq)), None)
                 .take(limit)
                 .map(|((_, event_seq), (digest, tx_digest, time))| {
                     (digest, tx_digest, event_seq, time)
@@ -1104,9 +1118,9 @@ impl IndexStore {
         Ok(if descending {
             self.tables
                 .event_order
-                .unbounded_iter()
-                .skip_prior_to(&(min(tx_seq, seq), event_seq))?
-                .reverse()
+                .reversed_safe_iter_with_bounds(None, Some((min(tx_seq, seq), event_seq)))?
+                .collect::<Result<Vec<_>, _>>()?
+                .into_iter()
                 .take_while(|((tx, _), _)| tx == &seq)
                 .take(limit)
                 .map(|((_, event_seq), (digest, tx_digest, time))| {
@@ -1116,8 +1130,7 @@ impl IndexStore {
         } else {
             self.tables
                 .event_order
-                .unbounded_iter()
-                .skip_to(&(max(tx_seq, seq), event_seq))?
+                .iter_with_bounds(Some((max(tx_seq, seq), event_seq)), None)
                 .take_while(|((tx, _), _)| tx == &seq)
                 .take(limit)
                 .map(|((_, event_seq), (digest, tx_digest, time))| {
@@ -1138,9 +1151,9 @@ impl IndexStore {
     ) -> SuiResult<Vec<(TransactionEventsDigest, TransactionDigest, usize, u64)>> {
         Ok(if descending {
             index
-                .unbounded_iter()
-                .skip_prior_to(&(key.clone(), (tx_seq, event_seq)))?
-                .reverse()
+                .reversed_safe_iter_with_bounds(None, Some((key.clone(), (tx_seq, event_seq))))?
+                .collect::<Result<Vec<_>, _>>()?
+                .into_iter()
                 .take_while(|((m, _), _)| m == key)
                 .take(limit)
                 .map(|((_, (_, event_seq)), (digest, tx_digest, time))| {
@@ -1149,8 +1162,7 @@ impl IndexStore {
                 .collect()
         } else {
             index
-                .unbounded_iter()
-                .skip_to(&(key.clone(), (tx_seq, event_seq)))?
+                .iter_with_bounds(Some((key.clone(), (tx_seq, event_seq))), None)
                 .take_while(|((m, _), _)| m == key)
                 .take(limit)
                 .map(|((_, (_, event_seq)), (digest, tx_digest, time))| {
@@ -1249,9 +1261,9 @@ impl IndexStore {
         Ok(if descending {
             self.tables
                 .event_by_time
-                .unbounded_iter()
-                .skip_prior_to(&(end_time, (tx_seq, event_seq)))?
-                .reverse()
+                .reversed_safe_iter_with_bounds(None, Some((end_time, (tx_seq, event_seq))))?
+                .collect::<Result<Vec<_>, _>>()?
+                .into_iter()
                 .take_while(|((m, _), _)| m >= &start_time)
                 .take(limit)
                 .map(|((_, (_, event_seq)), (digest, tx_digest, time))| {
@@ -1261,8 +1273,7 @@ impl IndexStore {
         } else {
             self.tables
                 .event_by_time
-                .unbounded_iter()
-                .skip_to(&(start_time, (tx_seq, event_seq)))?
+                .iter_with_bounds(Some((start_time, (tx_seq, event_seq))), None)
                 .take_while(|((m, _), _)| m <= &end_time)
                 .take(limit)
                 .map(|((_, (_, event_seq)), (digest, tx_digest, time))| {
@@ -1279,14 +1290,13 @@ impl IndexStore {
     ) -> SuiResult<impl Iterator<Item = Result<(ObjectID, DynamicFieldInfo), TypedStoreError>> + '_>
     {
         debug!(?object, "get_dynamic_fields");
-        let iter_lower_bound = (object, ObjectID::ZERO);
+        // The object id 0 is the smallest possible
+        let iter_lower_bound = (object, cursor.unwrap_or(ObjectID::ZERO));
         let iter_upper_bound = (object, ObjectID::MAX);
         Ok(self
             .tables
             .dynamic_field_index
             .safe_iter_with_bounds(Some(iter_lower_bound), Some(iter_upper_bound))
-            // The object id 0 is the smallest possible
-            .skip_to(&(object, cursor.unwrap_or(ObjectID::ZERO)))?
             // skip an extra b/c the cursor is exclusive
             .skip(usize::from(cursor.is_some()))
             .take_while(move |result| result.is_err() || (result.as_ref().unwrap().0 .0 == object))
@@ -1375,8 +1385,7 @@ impl IndexStore {
         let start_key =
             CoinIndexKey2::new(owner, starting_coin_type.clone(), u64::MAX, ObjectID::ZERO);
         Ok(coin_index
-            .unbounded_iter()
-            .skip_to(&start_key)?
+            .iter_with_bounds(Some(start_key), None)
             .take_while(move |(key, _)| {
                 if key.owner != owner {
                     return false;
@@ -1405,8 +1414,7 @@ impl IndexStore {
         Ok(self
             .tables
             .coin_index_2
-            .unbounded_iter()
-            .skip_to(&start_key)?
+            .iter_with_bounds(Some(start_key), None)
             .filter(move |(key, _)| key.object_id != starting_object_id)
             .enumerate()
             .take_while(move |(index, (key, _))| {
@@ -1435,9 +1443,8 @@ impl IndexStore {
         Ok(self
             .tables
             .owner_index
-            .unbounded_iter()
             // The object id 0 is the smallest possible
-            .skip_to(&(owner, starting_object_id))?
+            .iter_with_bounds(Some((owner, starting_object_id)), None)
             .skip(usize::from(starting_object_id != ObjectID::ZERO))
             .take_while(move |((address_owner, _), _)| address_owner == &owner)
             .filter(move |(_, o)| {

consensus/core/src/storage/rocksdb_store.rs
@@ -225,12 +225,10 @@ impl Store for RocksDBStore {
         let mut refs = VecDeque::new();
         for kv in self
             .digests_by_authorities
-            .safe_range_iter((
-                Included((author, Round::MIN, BlockDigest::MIN)),
-                Included((author, before_round, BlockDigest::MAX)),
-            ))
-            .skip_to_last()
-            .reverse()
+            .reversed_safe_iter_with_bounds(
+                Some((author, Round::MIN, BlockDigest::MIN)),
+                Some((author, before_round, BlockDigest::MAX)),
+            )?
             .take(num_of_rounds as usize)
         {
             let ((author, round, digest), _) = kv?;
@@ -247,7 +245,11 @@ impl Store for RocksDBStore {
     }
 
     fn read_last_commit(&self) -> ConsensusResult<Option<TrustedCommit>> {
-        let Some(result) = self.commits.safe_iter().skip_to_last().next() else {
+        let Some(result) = self
+            .commits
+            .reversed_safe_iter_with_bounds(None, None)?
+            .next()
+        else {
             return Ok(None);
         };
         let ((_index, digest), serialized) = result?;
@@ -289,7 +291,11 @@ impl Store for RocksDBStore {
     }
 
     fn read_last_commit_info(&self) -> ConsensusResult<Option<(CommitRef, CommitInfo)>> {
-        let Some(result) = self.commit_info.safe_iter().skip_to_last().next() else {
+        let Some(result) = self
+            .commit_info
+            .reversed_safe_iter_with_bounds(None, None)?
+            .next()
+        else {
             return Ok(None);
         };
         let (key, commit_info) = result.map_err(ConsensusError::RocksDBFailure)?;

crates/sui-core/src/authority.rs
@@ -2792,7 +2792,7 @@ impl AuthorityState {
             Some(seq) => self
                 .checkpoint_store
                 .get_checkpoint_by_sequence_number(seq)?,
-            None => self.checkpoint_store.get_latest_certified_checkpoint(),
+            None => self.checkpoint_store.get_latest_certified_checkpoint()?,
         }
         .map(|v| v.into_inner());
         let contents = match &summary {
@@ -2817,7 +2817,7 @@ impl AuthorityState {
                 Some(seq) => self
                     .checkpoint_store
                     .get_checkpoint_by_sequence_number(seq)?,
-                None => self.checkpoint_store.get_latest_certified_checkpoint(),
+                None => self.checkpoint_store.get_latest_certified_checkpoint()?,
             }
             .map(|v| v.into_inner());
             summary.map(CheckpointSummaryResponse::Certified)
@@ -2826,7 +2826,7 @@ impl AuthorityState {
                 Some(seq) => self.checkpoint_store.get_locally_computed_checkpoint(seq)?,
                 None => self
                     .checkpoint_store
-                    .get_latest_locally_computed_checkpoint(),
+                    .get_latest_locally_computed_checkpoint()?,
             };
             summary.map(CheckpointSummaryResponse::Pending)
         };
@@ -3171,7 +3171,7 @@ impl AuthorityState {
 
         let highest_locally_built_checkpoint_seq = self
             .checkpoint_store
-            .get_latest_locally_computed_checkpoint()
+            .get_latest_locally_computed_checkpoint()?
             .map(|c| *c.sequence_number())
             .unwrap_or(0);
 

crates/sui-core/src/authority/authority_per_epoch_store.rs
@@ -515,7 +515,7 @@ pub struct AuthorityEpochTables {
 
     /// Stores pending signatures
     /// The key in this table is checkpoint sequence number and an arbitrary integer
-    pending_checkpoint_signatures:
+    pub(crate) pending_checkpoint_signatures:
         DBMap<(CheckpointSequenceNumber, u64), CheckpointSignatureMessage>,
 
     /// Deprecated - pending signatures are now stored in memory.
@@ -701,22 +701,6 @@ impl AuthorityEpochTables {
         Ok(self.last_consensus_stats.get(&LAST_CONSENSUS_STATS_ADDR)?)
     }
 
-    pub fn get_pending_checkpoint_signatures_iter(
-        &self,
-        checkpoint_seq: CheckpointSequenceNumber,
-        starting_index: u64,
-    ) -> SuiResult<
-        impl Iterator<Item = ((CheckpointSequenceNumber, u64), CheckpointSignatureMessage)> + '_,
-    > {
-        let key = (checkpoint_seq, starting_index);
-        trace!("Scanning pending checkpoint signatures from {:?}", key);
-        let iter = self
-            .pending_checkpoint_signatures
-            .unbounded_iter()
-            .skip_to(&key)?;
-        Ok::<_, SuiError>(iter)
-    }
-
     pub fn get_locked_transaction(&self, obj_ref: &ObjectRef) -> SuiResult<Option<LockDetails>> {
         Ok(self
             .owned_object_locked_transactions
@@ -893,7 +877,7 @@ impl AuthorityPerEpochStore {
 
         let mut jwk_aggregator = JwkAggregator::new(committee.clone());
 
-        for ((authority, id, jwk), _) in tables.pending_jwks.unbounded_iter().seek_to_first() {
+        for ((authority, id, jwk), _) in tables.pending_jwks.unbounded_iter() {
             jwk_aggregator.insert(authority, (id, jwk));
         }
 
@@ -1176,9 +1160,9 @@ impl AuthorityPerEpochStore {
         Ok(self
             .tables()?
             .running_root_accumulators
-            .unbounded_iter()
-            .skip_to_last()
-            .next())
+            .reversed_safe_iter_with_bounds(None, None)?
+            .next()
+            .transpose()?)
     }
 
     pub fn insert_running_root_accumulator(
@@ -4126,11 +4110,10 @@ impl AuthorityPerEpochStore {
             // Reading from the db table is only need when upgrading to data quarantining
             // for the first time.
             let tables = self.tables()?;
-            let mut db_iter = tables.pending_checkpoints_v2.unbounded_iter();
-            if let Some(last_processed_height) = last {
-                db_iter = db_iter.skip_to(&(last_processed_height + 1))?;
-            }
-            db_iter.collect()
+            let db_iter = tables
+                .pending_checkpoints_v2
+                .safe_iter_with_bounds(last.map(|height| height + 1), None);
+            db_iter.collect::<Result<Vec<_>, _>>()?
         } else {
             vec![]
         };
@@ -4218,9 +4201,9 @@ impl AuthorityPerEpochStore {
         Ok(self
             .tables()?
             .builder_checkpoint_summary_v2
-            .unbounded_iter()
-            .skip_to_last()
+            .reversed_safe_iter_with_bounds(None, None)?
             .next()
+            .transpose()?
             .map(|(_, s)| s))
     }
 
@@ -4240,9 +4223,9 @@ impl AuthorityPerEpochStore {
             let seq = self
                 .tables()?
                 .builder_checkpoint_summary_v2
-                .unbounded_iter()
-                .skip_to_last()
+                .reversed_safe_iter_with_bounds(None, None)?
                 .next()
+                .transpose()?
                 .map(|(seq, s)| (seq, s.summary));
             debug!(
                 "returning last_built_summary from builder_checkpoint_summary_v2: {:?}",
@@ -4298,9 +4281,9 @@ impl AuthorityPerEpochStore {
         Ok(self
             .tables()?
             .pending_checkpoint_signatures
-            .unbounded_iter()
-            .skip_to_last()
+            .reversed_safe_iter_with_bounds(None, None)?
             .next()
+            .transpose()?
             .map(|((_, index), _)| index)
             .unwrap_or_default())
     }

crates/sui-core/src/authority/authority_store.rs
@@ -422,8 +422,7 @@ impl AuthorityStore {
             let marker_entry = self
                 .perpetual_tables
                 .object_per_epoch_marker_table_v2
-                .safe_iter_with_bounds(Some(min_key), Some(max_key))
-                .skip_prior_to(&max_key)?
+                .reversed_safe_iter_with_bounds(Some(min_key), Some(max_key))?
                 .next();
             match marker_entry {
                 Some(Ok(((epoch, key), marker))) => {
@@ -442,8 +441,7 @@ impl AuthorityStore {
             let marker_entry = self
                 .perpetual_tables
                 .object_per_epoch_marker_table
-                .safe_iter_with_bounds(Some(min_key), Some(max_key))
-                .skip_prior_to(&max_key)?
+                .reversed_safe_iter_with_bounds(Some(min_key), Some(max_key))?
                 .next();
             match marker_entry {
                 Some(Ok(((epoch, key), marker))) => {
@@ -564,10 +562,12 @@ impl AuthorityStore {
         let mut iterator = self
             .perpetual_tables
             .objects
-            .unbounded_iter()
-            .skip_prior_to(&ObjectKey(*object_id, prior_version))?;
+            .reversed_safe_iter_with_bounds(
+                Some(ObjectKey::min_for_id(object_id)),
+                Some(ObjectKey(*object_id, prior_version)),
+            )?;
 
-        if let Some((object_key, value)) = iterator.next() {
+        if let Some((object_key, value)) = iterator.next().transpose()? {
             if object_key.0 == *object_id {
                 return Ok(Some(
                     self.perpetual_tables.object_reference(&object_key, value)?,
@@ -620,10 +620,9 @@ impl AuthorityStore {
         let marker_entry = self
             .perpetual_tables
             .object_per_epoch_marker_table
-            .unbounded_iter()
-            .skip_prior_to(&marker_key)?
+            .reversed_safe_iter_with_bounds(None, Some(marker_key))?
             .next();
-        match marker_entry {
+        match marker_entry.transpose()? {
             Some(((epoch, key), marker)) => {
                 // Make sure object id matches and version is >= `version`
                 let object_data_ok = key.0 == *object_id && key.1 >= version;
@@ -1048,11 +1047,13 @@ impl AuthorityStore {
         let mut iterator = self
             .perpetual_tables
             .live_owned_object_markers
-            .unbounded_iter()
-            // Make the max possible entry for this object ID.
-            .skip_prior_to(&(object_id, SequenceNumber::MAX, ObjectDigest::MAX))?;
+            .reversed_safe_iter_with_bounds(
+                None,
+                Some((object_id, SequenceNumber::MAX, ObjectDigest::MAX)),
+            )?;
         Ok(iterator
             .next()
+            .transpose()?
             .and_then(|value| {
                 if value.0 .0 == object_id {
                     Some(value)
@@ -1819,8 +1820,7 @@ impl AccumulatorStore for AuthorityStore {
         Ok(self
             .perpetual_tables
             .root_state_hash_by_epoch
-            .safe_iter()
-            .skip_to_last()
+            .reversed_safe_iter_with_bounds(None, None)?
             .next()
             .transpose()?)
     }

commit 704f92e427003b49813013c5989f6aed1a763f10

[typed store] iterators: fix expensive callsites (#21406)
## Description 
This fixes expensive call sites caused by
0269b42498befb8f4cf628dd16b277d2e9d250dc.

---

## Release notes

Check each box that your changes affect. If none of the boxes relate to
your changes, release notes aren't required.

For each box you select, include information after the relevant heading
that describes the impact of your changes that a user might notice and
any actions they must take to implement updates.

- [ ] Protocol: 
- [ ] Nodes (Validators and Full nodes): 
- [ ] gRPC:
- [ ] JSON-RPC: 
- [ ] GraphQL: 
- [ ] CLI: 
- [ ] Rust SDK:

crates/sui-core/src/jsonrpc_index.rs
@@ -873,14 +873,17 @@ impl IndexStore {
                 )?
                 // skip one more if exclusive cursor is Some
                 .skip(usize::from(cursor.is_some()))
-                .collect::<Result<Vec<_>, _>>()?
-                .into_iter()
-                .take_while(|((id, _), _)| *id == key)
-                .map(|(_, digest)| digest);
+                .take_while(|result| {
+                    result
+                        .as_ref()
+                        .map(|((id, _), _)| *id == key)
+                        .unwrap_or(false)
+                })
+                .map(|result| result.map(|(_, digest)| digest));
             if let Some(limit) = limit {
-                iter.take(limit).collect()
+                iter.take(limit).collect::<Result<Vec<_>, _>>()?
             } else {
-                iter.collect()
+                iter.collect::<Result<Vec<_>, _>>()?
             }
         } else {
             let iter = index
@@ -1013,18 +1016,21 @@ impl IndexStore {
                 .reversed_safe_iter_with_bounds(None, Some(key))?
                 // skip one more if exclusive cursor is Some
                 .skip(usize::from(cursor.is_some()))
-                .collect::<Result<Vec<_>, _>>()?
-                .into_iter()
-                .take_while(|((id, m, f, _), _)| {
-                    *id == package
-                        && module.as_ref().map(|x| x == m).unwrap_or(true)
-                        && function.as_ref().map(|x| x == f).unwrap_or(true)
+                .take_while(|result| {
+                    result
+                        .as_ref()
+                        .map(|((id, m, f, _), _)| {
+                            *id == package
+                                && module.as_ref().map(|x| x == m).unwrap_or(true)
+                                && function.as_ref().map(|x| x == f).unwrap_or(true)
+                        })
+                        .unwrap_or(false)
                 })
-                .map(|(_, digest)| digest);
+                .map(|result| result.map(|(_, digest)| digest));
             if let Some(limit) = limit {
-                iter.take(limit).collect()
+                iter.take(limit).collect::<Result<Vec<_>, _>>()?
             } else {
-                iter.collect()
+                iter.collect::<Result<Vec<_>, _>>()?
             }
         } else {
             let iter = self
@@ -1119,14 +1125,19 @@ impl IndexStore {
             self.tables
                 .event_order
                 .reversed_safe_iter_with_bounds(None, Some((min(tx_seq, seq), event_seq)))?
-                .collect::<Result<Vec<_>, _>>()?
-                .into_iter()
-                .take_while(|((tx, _), _)| tx == &seq)
-                .take(limit)
-                .map(|((_, event_seq), (digest, tx_digest, time))| {
-                    (digest, tx_digest, event_seq, time)
+                .take_while(|result| {
+                    result
+                        .as_ref()
+                        .map(|((tx, _), _)| tx == &seq)
+                        .unwrap_or(false)
                 })
-                .collect()
+                .take(limit)
+                .map(|result| {
+                    result.map(|((_, event_seq), (digest, tx_digest, time))| {
+                        (digest, tx_digest, event_seq, time)
+                    })
+                })
+                .collect::<Result<Vec<_>, _>>()?
         } else {
             self.tables
                 .event_order
@@ -1152,14 +1163,14 @@ impl IndexStore {
         Ok(if descending {
             index
                 .reversed_safe_iter_with_bounds(None, Some((key.clone(), (tx_seq, event_seq))))?
-                .collect::<Result<Vec<_>, _>>()?
-                .into_iter()
-                .take_while(|((m, _), _)| m == key)
+                .take_while(|result| result.as_ref().map(|((m, _), _)| m == key).unwrap_or(false))
                 .take(limit)
-                .map(|((_, (_, event_seq)), (digest, tx_digest, time))| {
-                    (digest, tx_digest, event_seq, time)
+                .map(|result| {
+                    result.map(|((_, (_, event_seq)), (digest, tx_digest, time))| {
+                        (digest, tx_digest, event_seq, time)
+                    })
                 })
-                .collect()
+                .collect::<Result<Vec<_>, _>>()?
         } else {
             index
                 .iter_with_bounds(Some((key.clone(), (tx_seq, event_seq))), None)
@@ -1262,14 +1273,19 @@ impl IndexStore {
             self.tables
                 .event_by_time
                 .reversed_safe_iter_with_bounds(None, Some((end_time, (tx_seq, event_seq))))?
-                .collect::<Result<Vec<_>, _>>()?
-                .into_iter()
-                .take_while(|((m, _), _)| m >= &start_time)
-                .take(limit)
-                .map(|((_, (_, event_seq)), (digest, tx_digest, time))| {
-                    (digest, tx_digest, event_seq, time)
+                .take_while(|result| {
+                    result
+                        .as_ref()
+                        .map(|((m, _), _)| m >= &start_time)
+                        .unwrap_or(false)
                 })
-                .collect()
+                .take(limit)
+                .map(|result| {
+                    result.map(|((_, (_, event_seq)), (digest, tx_digest, time))| {
+                        (digest, tx_digest, event_seq, time)
+                    })
+                })
+                .collect::<Result<Vec<_>, _>>()?
         } else {
             self.tables
                 .event_by_time